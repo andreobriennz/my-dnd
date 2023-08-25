ccb2e2dde293601f2a6033798043db846ecdc72a
andreobriennz <andre.obrien877@gmail.com>
2023-08-25 18:48:49 +1200

    Remove redundant test

--- spec/controllers/registrations_controller_test.rb
+++ spec/controllers/registrations_controller_test.rb
@@ -11,11 +11,6 @@ RSpec.describe RegistrationsController, type: :controller do
                 password: 'password',
                 password_confirmation: 'password'
             }
-
-            @invalid_user_attributes = {
-                password: 'password',
-                password_confirmation: 'password'
-            }
         end
 
         context 'when the attributes are valid' do
@@ -26,16 +21,6 @@ RSpec.describe RegistrationsController, type: :controller do
 
                 expect(response).to redirect_to(root_path)
                 expect(session[:user_id]).to eq(User.last.id)
-            end
-        end
-
-        context 'when the attributes are invalid' do
-            it 'does not create a new user' do
-                expect {
-                    post :create, params: { user: @invalid_user_attributes }
-                }.to change(User, :count).by(0)
-
-                expect(session[:user_id]).to be_nil
             end
         end
     end
